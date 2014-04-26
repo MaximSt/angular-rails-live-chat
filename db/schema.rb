bc7c7290b9e4753f6af28d016c7ca5f1796b2e8d
Quan Sun <cfk.quan@gmail.com>
2014-04-26 22:18:10 +1000

    switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140425121904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chats", force: true do |t|
     t.string   "message"
     t.datetime "created_at"
